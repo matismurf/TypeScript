b46bd3672832735ce596de28381974a6eba5adfd
Tiasma01 <55673958+matismurf@users.noreply.github.com>
2022-11-20 11:34:15 -0300

    Add files via upload

--- tsc/src/index.ts
+++ tsc/src/index.ts
@@ -47,3 +47,191 @@ id = 200
 type myIdType = number | string
 const userId:myIdType = 10
 const productId:myIdType="00001"
+
+//enum
+//tamanho de roupas (size: médio, size: pequeno)
+enum Size{
+    P = "Pequeno",
+    M = "Médio",
+    G = "Grande"
+}
+
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.G
+}
+console.log(camisa);
+
+//literal types
+let teste: "autenticado" | null;
+
+teste = "autenticado";
+teste = null;
+
+//funcoes
+function sum(a:number, b:number){
+    return a + b;
+}
+
+console.log(sum(12,12));
+
+function sayHelloTo(name:string): string{
+    return `Hello ${name}`;
+}
+
+console.log(sayHelloTo("Matias"));
+
+function logger(msg: string):void{
+    console.log(msg);
+}
+
+logger("Teste");
+
+function greeting(name: string, greet?:string){
+    if(greet){
+        console.log(`Olá ${greet} ${name}`);
+    }else{
+        console.log(`Olá ${name}`)
+    }
+}
+
+greeting("José");
+greeting("Pedro", "Sr")
+
+//interfaces
+interface MathFunctionParams{
+    n1:number,
+    n2:number
+}
+
+function sumNumbers(nums:MathFunctionParams){
+    return nums.n1 + nums.n2
+}
+
+console.log(sumNumbers({n1:1,n2:2}));
+
+function multiplyNumbers(nums:MathFunctionParams){
+    return nums.n1 * nums.n2
+}
+
+const someNumbers:MathFunctionParams={
+    n1:5,
+    n2:10
+}
+
+console.log(multiplyNumbers(someNumbers))
+
+//narrowing
+//checagem tipos
+function doSomething(info: number | boolean){
+    if(typeof info === "number"){
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log('Não foi passado um número')
+}
+
+doSomething(5);
+doSomething(true);
+
+//generics
+function showArraysItems<T>(arr: T[]){
+    arr.forEach((item) => {
+        console.log(`Item: ${item}`)
+    })
+}
+
+const a1 =[1,2,3];
+const a2 =["a","b","c"];
+
+showArraysItems(a1);
+showArraysItems(a2);
+
+//classes
+class User {
+    name
+    role
+    isApproved
+
+    constructor(name:string,role:string,isApproved:boolean){
+        this.name = name
+        this.role = role
+        this.isApproved = isApproved
+    }
+
+    showUserName(){
+        console.log(`O nome do usuário é ${this.name}`)
+    }
+
+    showUserRole(canShow:boolean):void{
+        if(canShow){
+            console.log(`Função do usuário é: ${this.role}`)
+            return
+        }
+        console.log("Informação restrita")
+    }
+}
+
+const zeca = new User("Zéca","Admin",true)
+console.log(zeca);
+zeca.showUserName();
+zeca.showUserRole(false);
+
+//interfaces em classes
+interface IVehicle{
+    brand: string
+    showBrand():void
+}
+
+class Car implements IVehicle{
+    brand
+    wheels
+
+    constructor(brand:string, wheels:number){
+        this.brand = brand
+        this.wheels = wheels
+    }
+
+    showBrand(): void {
+        console.log(`A marca do carro é ${this.brand}`)
+    }
+}
+
+const fusca = new Car("VW", 4);
+
+fusca.showBrand();
+
+//herança
+class SuperCar extends Car{
+    engine
+
+    constructor(brand:string, wheels:number, engine:number){
+        super(brand, wheels)
+        this.engine = engine
+    }
+}
+
+const a4 = new SuperCar("Audi",4,2.0);
+console.log(a4);
+a4.showBrand();
+
+//decorators
+function BaseParameters(){
+    return function<T extends{new(...args:any[]): {}}>(constructor: T){
+        return class extends constructor{
+            id = Math.random()
+            createdAt = new Date();
+        }
+    }
+}
+
+@BaseParameters()
+class Person{
+    name
+    constructor(name:string){
+        this.name = name
+    }
+}
+
+const sam = new Person("Sam");
+console.log(sam);
